87a778191e60a4296af5a0c7edc80e4524a284c1
Bonface Njuguna <bonface.njuguna@learninglions.org>
2020-08-15 22:21:32 +0300

    shooting trial, not yet achieved

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(new Vector3(0, 10, 0) * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(new Vector3(0, -10, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(new Vector3(0, 0, 60) * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(new Vector3(0, 0, -60) * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/bulletshooter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletshooter : MonoBehaviour
+{
+
+    public GameObject Bullet_Emitter;
+
+    public GameObject Bullet;
+
+    public float Bullet_Forward_Force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            //The Bullet instantiation happens here.
+            GameObject Temporary_Bullet_Handler;
+            Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+            //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
+            //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
+            Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+           
+
+            //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
+            Destroy(Temporary_Bullet_Handler, 10.0f);
+        }
+    }
+}
