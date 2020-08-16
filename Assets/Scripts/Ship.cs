3cbaf53a98ff8d6b144880e8af1d3c9449efa2b7
Bonface Njuguna <bonface.njuguna@learninglions.org>
2020-08-17 02:00:12 +0300

    shooting not yet achieved

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -15,26 +15,23 @@ public class Ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 pos = transform.position;
-
         if (Input.GetKey("w"))
         {
-            transform.Translate(new Vector3(0, 10, 0) * Time.deltaTime);
+            transform.Translate(new Vector3(0, 0, 10) * Time.deltaTime);
         }
         if (Input.GetKey("s"))
         {
-            transform.Translate(new Vector3(0, -10, 0) * Time.deltaTime);
+            transform.Translate(new Vector3(0, 0, -10) * Time.deltaTime);
         }
 
         if (Input.GetKey("a"))
         {
-            transform.Rotate(new Vector3(0, 0, 60) * Time.deltaTime);
+            transform.Rotate(new Vector3(0, -60, 0) * Time.deltaTime);
         }
 
         if (Input.GetKey("d"))
         {
-            transform.Rotate(new Vector3(0, 0, -60) * Time.deltaTime);
+            transform.Rotate(new Vector3(0, 60, 0) * Time.deltaTime);
         }
     }
 }
---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            transform.Translate(Vector3.forward * speed);
+        }        
+    }
+}
